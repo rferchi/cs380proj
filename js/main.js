f068f6c8a6ae76931c9225d8095a7706fa581ad3
rferchi <144283389+rferchi@users.noreply.github.com>
2024-03-31 15:19:50 -0400

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,92 @@
+// An "account". Every object in here will be stored locally.
+class User
+{
+    // Multiple shipping addresses possible.
+    shippingInfo = [];
+
+    // Cart with items.
+    cart = null;
+
+    // Multiple payment methods possible.
+    paymentMethods = [];
+
+    // Set up the user!!!
+    constructor() 
+    {
+        // It's fine if these are null, it just means that there's no info for them yet
+        this.shippingInfo = window.localStorage.getItem( this.shippingInfo );
+        this.paymentMethods = window.localStorage.getItem( this.paymentMethods );
+
+        // Find a cart, if not then make a new cart.
+        this.cart = window.localStorage.getItem( this.cart );
+        if ( this.cart )
+        {
+            cart = new Cart();
+        }
+        console.log( this );
+    }
+}
+
+
+// Item object constructor
+function Item( _name, _description, _price, _sale )
+{
+    this.name = _name;
+    this.description = _description;
+    this.price = _price;
+    this.sale = _sale;
+}
+
+class Cart
+{
+    constructor( storageObj )
+    {
+        // Extract from the storage object
+
+    }
+
+    AddToCart( ...Item )
+    {
+        this.Items.push( ...Item );
+    }
+
+    RemoveFromCart( ...Item )
+    {
+        for ( let i = 0; i < Item.length; i++ )
+        {
+            if ( Item[i] == this.Items.find( Item ) )
+            {
+                //this.Items.
+            }
+        }
+    }
+
+    // Call this function to calculate the subtotal.
+    CalculateSubtotal()
+    {
+        let result = 0;
+
+        for ( let i = 0; i < this.Items.length; i++ )
+        {
+            result += this.Items[i].price; 
+        }
+
+        subtotal = result;
+    }
+
+    Items = [];
+
+    // Calculated value based on everything in the list.
+    subtotal = 0;
+
+}
+
+
+// Store these if the user wants it to be stored
+class ShippingInfo {
+
+}
+
+class PaymentInfo {
+
+}
